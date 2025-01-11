2b18b2ab06de9ff6dfcc17f4e4aca5ce0f9073b9
Tarkas <tarkas.679@gmail.com>
2025-01-11 08:36:55 +0000

    Initial bot files

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from threading import Thread
+import logging
 
 app = Flask('')
+# Disable Flask logging except for errors
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 @app.route('/')
 def home():
     return "I'm alive"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    server = Thread(target=run)
+    server.daemon = True  # This ensures the thread will close when the main program ends
+    server.start()
